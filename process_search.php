70825ba090d476d86ddf4f47b6cb55b6050a2da0
xcloudx8 <danielestrife@gmail.com>
2016-11-07 13:36:43 +0100

    V2.0.1

--- process_search.php
+++ process_search.php
@@ -148,7 +148,7 @@
           // printing table headers
           for($i=0; $i<$fields_num; $i++)
           {
-              $field = pg_fetch_result($result);
+              $field = pg_fetch_result($result, 1);
               echo "<td>{$field->name}</td>";
           }
           echo "</tr>\n";
