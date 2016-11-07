5669c97b03780c71b190c17c6dcbc307e87d6bbc
xcloudx8 <danielestrife@gmail.com>
2016-11-07 10:52:04 +0100

    V2.0.1

--- process_search.php
+++ process_search.php
@@ -25,7 +25,36 @@
     </div>
     <main class="mdl-layout__content">
       <div class="page-content">
-          Contents here!
+        <?php
+            $servername = "157.27.254.179";
+            $username = "daniele";
+            $password = "Cloudtesi8!";
+            $dbname = "genomes";
+
+            // Create connection
+            $conn = new pg_connect($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT * FROM project_infos";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo "<table><tr><th>ID</th><th>Name</th></tr>";
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+            ?>
+
+
       </div>
     </main>
   </div>
