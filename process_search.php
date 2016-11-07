7660b21008d39ffc7a138c9631356e9067e7c67d
xcloudx8 <danielestrife@gmail.com>
2016-11-07 10:25:52 +0100

    V2.0.1

--- process_search.php
+++ process_search.php
@@ -95,7 +95,36 @@
             }
 
           </style>
-
+          <?php
+            function sql_to_html_table($sqlresult, $delim="\n") {
+              // starting table
+              $htmltable =  "<table>" . $delim ;
+              $counter   = 0 ;
+              // putting in lines
+              while( $row = $sqlresult->fetch_assoc()  ){
+                if ( $counter===0 ) {
+                  // table header
+                  $htmltable .=   "<tr>"  . $delim;
+                  foreach ($row as $key => $value ) {
+                      $htmltable .=   "<th>" . $key . "</th>"  . $delim ;
+                  }
+                  $htmltable .=   "</tr>"  . $delim ;
+                  $counter = 22;
+                }
+                  // table body
+                  $htmltable .=   "<tr>"  . $delim ;
+                  foreach ($row as $key => $value ) {
+                      $htmltable .=   "<td>" . $value . "</td>"  . $delim ;
+                  }
+                  $htmltable .=   "</tr>"   . $delim ;
+              }
+              // closing table
+              $htmltable .=   "</table>"   . $delim ;
+              // return
+              return( $htmltable ) ;
+            }
+            echo sql_to_html_table( $result, $delim="\n" ) ; 
+?>
 
           <table>
             <tr>
