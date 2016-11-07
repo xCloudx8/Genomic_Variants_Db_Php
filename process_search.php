2fc99be789d32e504249b02db12b5e966bb8e0cc
xcloudx8 <danielestrife@gmail.com>
2016-11-07 13:35:02 +0100

    V2.0.1

--- process_search.php
+++ process_search.php
@@ -126,38 +126,16 @@
               margin-top: 30px;
               margin-left: 15px;
             }
-
-            table:first-child{
-              border-top: 1px solid grey;
-              border-bottom: 1px solid grey;
-            }
-            table:last-child{
-              border-bottom: 0px solid #e0e0e0;
-            }
-
             th {
               background: #e0e0e0;
               color: #e0e0e0;
               font-weight: bold;
-            }
-            th:first-child{
-              border-top: 1px solid #e0e0e0;
-            }
-            th:last-child{
-              border-bottom: 0px solid #e0e0e0;
             }
             td, th {
               padding: 5px;
               text-align: left;
               border-right: solid 1px #e0e0e0;
               border-left: solid 1px #e0e0e0;
-            }
-            tr:first-child{
-              border-top: 0px solid #e0e0e0;
-              border-bottom: 1px solid #e0e0e0;
-            }
-            tr:last-child{
-              border-bottom: 0px solid #e0e0e0;
             }
             tr:hover{
               background: #eeeeee;
