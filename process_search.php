72ac174f1bd163ad593958c5e3af90e121de383c
xcloudx8 <danielestrife@gmail.com>
2016-11-07 10:42:14 +0100

    V2.0.1

--- process_search.php
+++ process_search.php
@@ -121,40 +121,6 @@
           pg_free_result($result);
         ?>
       </table>
-
-      <p>
-        <?php
-         try {
-           //first pass just gets the column names
-           print "<table> n";
-           $result = $dbconn->pg_fetch_all_columns($query);
-           //return only the first row (we only need field names)
-           $row = $result->pg_fetch_row(PDO::FETCH_ASSOC);
-           print " <tr> n";
-           foreach ($row as $field => $value){
-            print " <th>$field</th> n";
-           } // end foreach
-           print " </tr> n";
-           //second query gets the data
-           $data = $con->query($query);
-           $data->setFetchMode(PDO::FETCH_ASSOC);
-           foreach($data as $row){
-            print " <tr> n";
-            foreach ($row as $name=>$value){
-            print " <td>$value</td> n";
-            } // end field loop
-            print " </tr> n";
-           } // end record loop
-           print "</table> n";
-         } catch(PDOException $e) {
-          echo 'ERROR: ' . $e->getMessage();
-         } // end try
-        ?>
-      </p>
-
-
-
-
       </div>
     </main>
   </div>
