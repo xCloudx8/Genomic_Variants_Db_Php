fe2a9131afe136a1ff374371ef17ffb933a367d2
xcloudx8 <danielestrife@gmail.com>
2016-11-07 14:37:04 +0100

    V2.0.1

--- process_search.php
+++ process_search.php
@@ -152,7 +152,7 @@
               {
                   $name = pg_query("SELECT * FROM project_infos limit 1 ");
                   $field = pg_fetch_row($result);
-                  echo "<td>$field</td>";
+                  echo "<td>{$field->name}</td>";
               }
               echo "</tr>\n";
               // printing table rows
